e1e6bb0edfccc13628ddeaa749b820c2533fcdbe
MONSTER ABRAA5-9.2.2 <MONSTER ABRAA5-9.2.2@DESKTOP-OKVCD72>
2021-11-30 01:32:57 +0300

    30.11.2021

---
+++ ClassDemoMetot/Musteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDemoMetot
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyad { get; set; }
+        public int TelNo { get; set; }
+    }
+}
---
+++ ClassDemoMetot/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDemoMetot
+{
+    class MusteriManager
+    {
+        public void MusteriAdd(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri adı : " + musteri.Adi + " " + "Müşteri Soyadı : " + musteri.Soyad + " " + " Telefon Numarası : " + musteri.TelNo + "müşteri eklenmiştir.");
+        }
+
+        public void MusteriDel(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri adı : " + musteri.Adi + " " + "Müşteri Soyadı : " + musteri.Soyad + " " + " Telefon Numarası : " + musteri.TelNo + " müşteri silinmiştir.");
+        }
+
+        public void MusteriList(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri adı : " + musteri.Adi + " " + "Müşteri Soyadı : " + musteri.Soyad + " " + " Telefon Numarası : " + musteri.TelNo + "müşteri listelenmiştir.");
+        }
+    }
+}
