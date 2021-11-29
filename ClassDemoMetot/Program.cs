8a17150a85f914cc6568c278acb9ea35fdefade1
MONSTER ABRAA5-9.2.2 <MONSTER ABRAA5-9.2.2@DESKTOP-OKVCD72>
2021-11-30 01:02:40 +0300

    Add project files.

---
+++ ClassDemoMetot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassDemoMetot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
