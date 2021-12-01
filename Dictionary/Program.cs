cdc6a9dbcfdfcebe6cd06babbfa1cce6695d2c45
MONSTER ABRAA5-9.2.2 <MONSTER ABRAA5-9.2.2@DESKTOP-OKVCD72>
2021-12-01 07:31:17 +0300

    1.12.2021

---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Veli");
+            myDictionary.Add(1, "Veli");
+            myDictionary.Add(1, "Veli");
+            myDictionary.Add(1, "Veli");
+            Console.WriteLine(myDictionary.Count);
+        }
+    }    
+}
